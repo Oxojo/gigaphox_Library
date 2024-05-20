8f4ae8efebb19dd201cacad633f0902c31f72eb2
Oxojo <166904252+Oxojo@users.noreply.github.com>
2024-05-21 05:00:57 +0900

    Create Bellman-Ford.cpp

---
+++ Graph/Bellman-Ford.cpp
@@ -0,0 +1,15 @@
+vector<ll> bellman_ford(Edges &edges, ll V, ll s) {
+    vector<ll> dist(V, INF);
+    dist[s] = 0;
+    for (ll i = 0; i < V - 1; i++) {
+        for (auto e : edges) {
+            if (dist[e.from] == INF) continue;
+            dist[e.to] = min(dist[e.to], dist[e.from] + e.cost);
+        }
+    }
+    for (auto e : edges) {
+        if (dist[e.from] == INF) continue;
+        if (dist[e.from] + e.cost < dist[e.to]) return vector<T>();
+    }
+    return dist;
+}

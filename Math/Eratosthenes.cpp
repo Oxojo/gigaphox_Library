7a23f8f1e8ee68d357df069f47568e4bcb58cc7a
Oxojo <166904252+Oxojo@users.noreply.github.com>
2024-05-21 16:13:15 +0900

    Update Eratosthenes.cpp

--- Math/Eratosthenes.cpp
+++ Math/Eratosthenes.cpp
@@ -1,24 +1,25 @@
 struct Eratosthenes {
     vector<bool>prime;
+    vector<ll>p;
 
-    Eratosthenes(ll N) : prime(N + 1, true) {
+    Eratosthenes(ll N) : prime(N + 1, true), p(N + 1, 0) {
         prime[0] = prime[1] = false;
         for (ll i = 2; i <= N; i++) {
             if (!prime[i]) continue;
+            p[i]++;
             ll tmp = 2 * i;
             while (tmp <= N) {
                 prime[tmp] = false;
                 tmp += i;
             }
         }
+        for (ll i = 1; i <= N; i++) {
+            p[i] += p[i - 1];
+        }
     }
 
-    ll count(ll limit) { //[1, limit)
-        assert(limit <= (ll)prime.size());
-        ll sum = 0;
-        for (ll i = 0; i < limit; i++) {
-            sum += prime[i];
-        }
-        return sum;
+    ll count(ll limit) { //[1, limit]
+        assert(limit <= N);
+        return p[limit];
     }
 };

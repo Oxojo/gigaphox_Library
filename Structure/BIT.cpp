33aa165e46f7a49cb532e7b871e1c54b7d434032
Oxojo <166904252+Oxojo@users.noreply.github.com>
2024-06-18 02:50:51 +0900

    Update BIT.cpp

--- Structure/BIT.cpp
+++ Structure/BIT.cpp
@@ -1,19 +1,51 @@
+template <typename T>
 struct BIT {
     int N;
-    vector<ll>A;
-    BIT(int N_) : N(N_ + 1), A(N, 0) {}
+    vector<T>data;
+
+		BIT() = default;
+    explicit BIT(ll N_) : N(N_ + 1), data(N + 1, T()) {}
+		explicit BIT(vector<T> &v) : BIT((ll)v.size()) { build(v); }
     
-    void add(int i, ll x) {
-        for (int idx = i; idx < N; idx += (idx & -idx)) {
-            A[idx] += x;
-        }
-    }
-    
-    ll sum(int i) {
-        s = 0;
-        for (int idx = i; idx > 0; idx -= (idx & -idx)) {
-            s += A[idx];
-        }
-        return s;
-    }
+    void build(vector<T> &v) {
+			rep(i, N) data[i + 1] = v[i];
+			reps(i, 1, N + 1) {
+				ll j = i + (i & -i);
+				if (j <= N) data[j] += data[i];
+			}
+		}
+		
+		void apply(ll k, T &x) {
+			for (++k; k <= N; k += k & -k) data[k] += x;
+		}
+
+		T prod(ll r) const {
+			T ret = T();
+			for (; r > 0; r -= r & -r) ret += data[r];
+			return ret;
+		}
+
+		T prod(ll l, ll r) const { return prod(r) - prod(l); }
+
+		ll lower_bound(T x) const {
+			ll i = 0;
+			for (ll k = 1 << (__lg(N) + 1); k > 0; k >>= 1) {
+				if (i + k <= N && data[i + k] < x) {
+					x -= data[i + k];
+					i += k;
+				}
+			}
+			return i;
+		}
+
+		ll upper_bound(T x) const {
+			ll i = 0;
+			for (ll k = 1 << (__lg(N) + 1); k > 0; k >>= 1) {
+				if (i + k <= N && data[i + k] <= x) {
+					x -= data[i + k];
+					i += k;
+				}
+			}
+			return i;
+		}
 };
